edfcac019ed33ec0e37029255ef75499cfff23af
snegicodes <snegicodesmain@gmail.com>
2025-04-24 02:39:07 +0530

    update: meta data

--- app/page.js
+++ app/page.js
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-8">
       <h1 className="gradient-title font-extrabold text-3xl md:text-6xl tracking-tight md: px-6 text-center">
-        Thief Detection Alarm
+        Silent Sentinel 
       </h1>
+      <h2 className="gradient-title font-bold text-xl md:text-2xl tracking-tight md: px-6 text-center mt-2">
+        Theft Detection Alarm
+      </h2>
       <ObjectDetection/>
     </main>
   );
